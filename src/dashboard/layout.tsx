5638b0b7e5fd0c8398785f8f3c15096e8c299eb6
BabaVo55 <ethemcode@outlook.com>
2025-07-07 16:26:53 +0100

    title extraction

---
+++ src/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        Hello
+        {children}
+      </body>
+    </html>
+  );
+}
