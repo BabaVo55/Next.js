5cb6c1792373c8addb3d2bd2685f90b2b4481867
BabaVo55 <ethemcode@outlook.com>
2025-07-08 15:52:55 +0100

    testing scroll with for loop didnt work so we opted for Array instead

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -134,8 +134,14 @@ export default function DashboardLayout({
           </div>
 
            {/* Right */}
-          <div className=" w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] ">R</div>
-          {children}
+          <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+            {Array.from({ length: 1000 }, (_, i) => (
+              <div key={i}>
+                <p>{i}</p>
+              </div>
+            ))}
+          </div>
+          {/* {children} */}
         </div>
   );
 }
